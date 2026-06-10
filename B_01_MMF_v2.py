4ec78ebaf7dcaaff6577fa9551d9f05e1ff6be0d
irichardsmmc <isaacrichards@mmc.school.nz>
2026-06-10 12:24:17 +1200

    Panda integrated into base

--- B_01_MMF_v2.py
+++ B_01_MMF_v2.py
@@ -2,34 +2,11 @@ import pandas
 
 
 # Functions
-def int_check(question):
-    """Checks users enter an integer / float that is more than
-    zero (or the optional exit code)"""
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
 
-    error = "Oops - please enter an integer more than zero."
-
-    while True:
-
-        try:
-            # Return the response if it's an integer
-            response = int(input(question))
-
-            return response
-
-        except ValueError:
-            print(error)
-
-
-def not_blank(question):
-    """Checks that a user response is not blank"""
-
-    while True:
-        response = input(question)
-
-        if response != "":
-            return response
-
-        print("Sorry, this can't be blank. Please try again.\n")
+    print(f"{decoration * 3} {statement} {decoration * 3}")
 
 
 def string_check(question, valid_answers=('yes', 'no'), num_letters=1):
@@ -52,12 +29,69 @@ def string_check(question, valid_answers=('yes', 'no'), num_letters=1):
         print(f"Please choose an option from {valid_answers}")
 
 
+def instructions():
+    make_statement("Instructions", "ℹ")
+
+    print('''
+    
+For each ticket holder enter ... 
+- Their name
+- Their age
+- The payment method (cash/credit)
+
+The program will record the ticket sale and calculate the 
+ticket cost (and the profit).
+
+Once you have either sold all of the tickets or entered the 
+exit code ('xxx'), the program will display the ticket 
+sales information and write the data to a text file.
+
+It will also choose one lucky ticket holder who wins the 
+draw (their ticket is free).
+
+    ''')
+
+
+def not_blank(question):
+    """Checks that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this can't be blank. Please try again.\n")
+
+
+def int_check(question):
+    """Checks users enter an integer / float that is more than
+    zero (or the optional exit code)"""
+
+    error = "Oops - please enter an integer more than zero."
+
+    while True:
+
+        try:
+            # Return the response if it's an integer
+            response = int(input(question))
+
+            return response
+
+        except ValueError:
+            print(error)
+
+
 def currency(x):
     """Formats numbers as currency ($#.##)"""
     return "${:.2f}".format(x)
 
 
 # Main Routine
+
+# Initialise ticket numbers
+MAX_TICKETS = 5
+tickets_sold = 0
 
 # initialise variables / non-default options for string checker
 payment_ans = ('cash', 'credit')
@@ -81,12 +115,26 @@ mini_movie_dict = {
     'Surcharge': all_surcharges
 }
 
-# Get Ticket Details
-while True:
-    print()
+# Program main heading
+make_statement("Mini-Movie Fundraiser Program", "🍿")
 
+# Ask user if they want to see the instructions
+# display them if necessary
+print()
+want_instructions = string_check("Do you want to see the instructions? ")
+
+if want_instructions == "yes":
+    instructions()
+
+print()
+
+# Loop to get name, age, and payment details
+while tickets_sold < MAX_TICKETS:
     # ask user for name (check it's not blank)
+    print()
     name = not_blank("Name: ")
+
+    # if name is exit code, break out of loop
     if name == "xxx":
         break
 
@@ -128,6 +176,9 @@ while True:
     all_ticket_costs.append(ticket_price)
     all_surcharges.append(surcharge)
 
+    tickets_sold += 1
+
+# End of Ticket Loop
 # create dataframe / table from dictionary
 mini_movie_frame = pandas.DataFrame(mini_movie_dict)
 
@@ -149,3 +200,8 @@ print(mini_movie_frame.to_string(index=False))
 print()
 print(f"Total Paid: ${total_paid:.2f}")
 print(f"Total Profit: ${total_profit:.2f}")
+
+if tickets_sold == MAX_TICKETS:
+    print(f"You have sold all the tickets (ie: {MAX_TICKETS} tickets)")
+else:
+    print(f"You have sold {tickets_sold} / {MAX_TICKETS} tickets.")
